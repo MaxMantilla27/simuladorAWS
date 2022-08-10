3313524dee87e58be495a0885147bee26676e301
Max Mantilla Rodríguez <mmantilla@bsginstitute.com>
2022-08-10 16:55:55 -0500

    actualizacion https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  url_api:'http://simuladoraws-servicio.bsginstitute.com/api/',
+  url_api:'https://simuladoraws-servicio.bsginstitute.com/api/',
 };
