9a67f93791978e5fce8b84bc9d41c01292c1cf11
Max Mantilla Rodríguez <mmantilla@bsginstitute.com>
2022-08-10 16:22:36 -0500

    verificación

--- src/app/auth/Guard/auth.guard.ts
+++ src/app/auth/Guard/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivateChild, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { SessionStorageService } from 'src/app/shared/Services/session-storage.service';
 
